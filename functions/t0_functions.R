7505cd48b4121fb68448f6d65f1339fc75af5ded
Rafael Silva <rafael.rochasil@gmail.com>
2019-12-21 20:19:37 -0200

    back to it... sending progress to the cloud

--- functions/t0_functions.R
+++ functions/t0_functions.R
@@ -13,6 +13,7 @@ get.data <- function() {
         download.file("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip",
                       "data/Coursera-SwiftKey.zip")
     }
+    ## Extracting
     if(!dir.exists("data/final")) unzip("data/Coursera-SwiftKey.zip", exdir = "data")
 }
 
@@ -21,18 +22,23 @@ get.data <- function() {
 ##   - lang: the language in which you want to read that can be "en_US", "de_DE", 
 ##           "fi_FI" and "ru_RU" for English, German, Finnish and Russian 
 ##           respectively.
-##   - source: from which source you want to read you can choose one or more from
-##             "blogs", "news"and "twitter".
+##   - s: from which source you want to read you can choose one or more from
+##        "blogs", "news"and "twitter".
 ##   - n: The number of lines to read from each file. n = -1L means that all the 
 ##        lines should be read.
 ## This function returns a list that contains a list element for each of the read
 ## files.
-read.data <- function(lang="en_US", Source=c("blogs", "news", "twitter"), n=-1L) {
+read.data <- function(lang = "en_US", s = c("blogs", "news", "twitter"), n = -1L) {
+    ## Setting source directory
     directory <- paste("./data/final/", lang, "/", sep="")
+    ## Creating output list
     out = list()
-    for (i in 1:length(Source)) {
-        con <- file(paste(directory, lang, ".", Source[i], ".txt", sep=""))
-        out[[Source[i]]] <- readLines(con, n=n, warn=FALSE)
+    for (i in 1:length(s)) {
+        ## Opening connection
+        con <- file(paste(directory, lang, ".", s[i], ".txt", sep=""))
+        ## Reading from connection and storing in the output list
+        out[[s[i]]] <- readLines(con, n = n, warn = FALSE)
+        ## Closing connection
         close(con)
     }
     return(out)
--- functions/t1_functions.R
+++ functions/t1_functions.R
@@ -3,12 +3,51 @@
 ## This script has all the functions created to accomplish the task 1, they will
 ## be used in the other scripts and reports
 
-to_corpus <- function(X, lang = NULL, Source=c("blogs", "news", "twitter")){
-    out = list()
-    for(i in 1:length(Source)){
-        out[[Source[i]]] <- corpus(X[[Source[i]]])
-        metadoc(out[[Source[i]]], "language") <- lang
-        metadoc(out[[Source[i]]], "source") <- paste(lang, Source[i], 1:ndoc(out[[Source[i]]]), sep = ".")
+# to_df
+## to_df transforms the list outputed by the read.data function into a dataframe.
+to_df <- function(lists, lang){
+    ## Loading dplyr package
+    require(dplyr, quietly = TRUE)
+    ## Creating output data frame.
+    out <- data.frame(ID = character(), 
+                      Text = character(),
+                      stringsAsFactors = FALSE)
+    ## Transforming the data
+    for(i in names(lists)){
+        newdf <- data.frame(ID = paste(i, lang, 1:length(lists[[i]]), sep = "."),
+                            stringsAsFactors = FALSE)
+        newdf["Text"] <- as.character(lists[[i]])
+        ## Merging data from different sources
+        out <- bind_rows(out, newdf)
     }
-    return(out)
+    return(data.frame(out))
 }
+
+# ptonkens
+## function for paralellized tokenization
+ptokens <- function(docs, dnames, ncl, tolower = FALSE, ...){
+    ## Oppening parallel backend
+    cl <- makeCluster(ncl)
+    registerDoParallel(cl)
+    registerDoSEQ()
+    ## tolower
+    if(tolower) docs <- tolower(docs)
+    ## Separating chunks
+    suppressWarnings(lists <- split(docs, 1:ncl))
+    suppressWarnings(names <- split(dnames, 1:ncl))
+    ## Tokenizing
+    out <- foreach(i = 1:ncl, .combine = "+", .packages = "quanteda") %dopar% {
+        toks <- tokens(lists[[i]], ...)
+        names(toks) <- names[[i]]
+        toks
+    }
+    ## Closing connection
+    stopCluster(cl)
+    ##Returning output
+    out
+}
+
+
+
+
+
